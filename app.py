f7c52512dca3c9be1ef937536c71b2736a0593a9
Manu Musy <manuelmusy8@gmail.com>
2025-01-16 09:13:03 +0100

    add app.py file

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Mon application avec Streamlit")
+st.header("Introduction")
+st.write("Ceci est un texte affiché par Streamlit.")
+st.write("Vous pouvez également afficher des données structurées :", {"clé": "valeur"})
